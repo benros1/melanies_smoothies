d9ff973e6acb5902d7a280fee6e9b482445ee492
benros1 <benros1@gmail.com>
2024-10-21 17:52:13 +0300

    Update streamlit_app.py
    fixing pd_df not found

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         # fruity_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
         # fv_df = st.dataframe(data=fruity_responce.json(), use_container_width=True)
-        # pd_df = fv_df.to_pandas()
+        pd_df = my_dataframe.to_pandas()
         ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
